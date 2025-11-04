32356c1e66fc0fd1dc2a86e09508ee37d4058ac0
Viktoriia <viktoriamyhailiak@gmail.com>
2025-11-04 20:58:50 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from Rent Apartments Backend!");
+});
+
+const PORT = process.env.PORT || 3005;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
