13cd23e6fbbb8f6811ee010c940cab838c7a7102
Viktoriia <viktoriamyhailiak@gmail.com>
2025-11-13 15:24:39 +0200

    add a solution

--- dist/routes/apartments.route.js
+++ dist/routes/apartments.route.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getAllApartments } from '../controllers/apartments.controller';
+import { getAllApartments } from '../controllers/apartments.controller.ts';
 const apartmentsRouter = Router();
 apartmentsRouter.get('/', getAllApartments);
 export default apartmentsRouter;
