832194bf24d5845842e81165d353af56456f94b6
Viktoriia <viktoriamyhailiak@gmail.com>
2025-11-21 22:16:02 +0200

    change controller

--- src/controllers/apartments.controller.ts
+++ src/controllers/apartments.controller.ts
@@ -1,22 +1,24 @@
-import type { Request, Response } from 'express';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import type { Request, Response } from "express";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-
-const apartmentsENPath = path.resolve(__dirname, '../data/apartmentsEN.json');
-const apartmentsEN = JSON.parse(fs.readFileSync(apartmentsENPath, 'utf-8'));
-const apartmentsUAPath = path.resolve(__dirname, '../data/apartmentsUA.json');
-const apartmentsUA = JSON.parse(fs.readFileSync(apartmentsUAPath, 'utf-8'));
+const apartmentsENPath = path.resolve(__dirname, "../data/apartmentsEN.json");
+const apartmentsEN = JSON.parse(fs.readFileSync(apartmentsENPath, "utf-8"));
+const apartmentsUAPath = path.resolve(__dirname, "../data/apartmentsUA.json");
+const apartmentsUA = JSON.parse(fs.readFileSync(apartmentsUAPath, "utf-8"));
 
 export const getAllApartments = (req: Request, res: Response) => {
   const lang = req.query.lang;
 
-  if (lang?.toString().toLocaleLowerCase() === 'ua') {
+  if (
+    lang?.toString().toLocaleLowerCase() === "ua" ||
+    lang?.toString().toLocaleLowerCase() === "uk"
+  ) {
     res.json(apartmentsUA);
   } else {
     res.json(apartmentsEN);
